9c5f9125a27b45e72d45dde8c799bf602e0c95c9
ilmiabdillah199-beep <ilmiabdillah199@gmail.com>
2025-11-04 10:42:48 +0700

    Add files via upload

---
+++ Mavenproject3.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author acer
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        
+        for(a=1; a<=5; a++){
+            for(b=a; b<=5; b++){
+                System.out.print(" ");
+            }
+            for(c=1; c<=(2*a-1); c++){
+                System.out.print("*");
+            }
+            System.out.println("");
+            
+// 
+        }
+        
+        
+        
+        
+        
+  import teme
+          
+    }
+}
